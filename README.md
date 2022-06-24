4f161bf27b1044cbb525587183752bc014cff865
Shinichi Tokunaga <deepneko@ShinichinoMacBook-Pro.local>
2022-06-25 08:01:11 +0900

    Update README.

---
+++ README.md
@@ -0,0 +1,2 @@
+Codes to study Hacker's Delight
+https://en.wikipedia.org/wiki/Hacker%27s_Delight
--- cp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-cd $1
-make clean
-cd ../
-cp -R $1 $2
-sed s/$3/$4/ $1/Makefile > $2/Makefile
-mv $2/$3.c $2/$4.c
